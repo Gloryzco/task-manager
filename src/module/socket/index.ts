0eaf4b6ca44bad81769f3d047beb28db699981d3
Glory Andrew <andrewglory32@gmail.com>
2024-05-19 17:55:12 +0100

    unit tests implementation

--- src/module/socket/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './gateways';
-export * from './socket.module';
--- src/module/socket/socket.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SocketGateway } from './gateways';
-import { TaskModule } from '../task';
-
-@Module({
-  imports: [TaskModule],
-  providers: [SocketGateway],
-})
-export class SocketModule {}
